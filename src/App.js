0bcd7709dc8931ed981e5afa3155863217cdaab5
Sebastian Parias <sebastianpariasarias@gmail.com>
2023-02-28 11:13:42 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,57 +2,23 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import React, { useState } from "react";
 
-// to-do: components
+const dias = [
+  1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22,
+  23, 24, 25, 26, 27, 28, 29, 30, 31,
+];
 
-// import { DateTime } from "luxon";
-
-// const dt = new DateTime("12:01");
-// const dt2 = new DateTime("12:02");
-// console.log(dt)
-// console.log(dt2)
-
-const dias = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
-
-const formularioInicial = {
-  entradaDia1: "",
-  salidaDia1: "",
-  entradaDia2: "",
-  salidaDia2: "",
-  entradaDia3: "",
-  salidaDia3: "",
-  entradaDia4: "",
-  salidaDia4: "",
-  entradaDia5: "",
-  salidaDia5: "",
-  entradaDia6: "",
-  salidaDia6: "",
-  entradaDia7: "",
-  salidaDia7: "",
-  entradaDia8: "",
-  salidaDia8: "",
-  entradaDia9: "",
-  salidaDia9: "",
-  entradaDia10: "",
-  salidaDia10: "",
-  entradaDia11: "",
-  salidaDia11: "",
-  entradaDia12: "",
-  salidaDia12: "",
-  entradaDia13: "",
-  salidaDia13: "",
-  entradaDia14: "",
-  salidaDia14: "",
-  entradaDia15: "",
-  salidaDia15: "",
-};
+const formularioInicial = {};
+for (let i = 1; i <= 31; i++) {
+  formularioInicial[`entradaDia${i}`] = "";
+  formularioInicial[`salidaDia${i}`] = "";
+}
 
 function App() {
-  // const [inicioRecargo, setInicioRecargo] = useState("");
-  // const [finRecargo, setFinRecargo] = useState("");
   const [jornadaLaboral, setJornadaLaboral] = useState(9);
   const [formulario, setFormulario] = useState(formularioInicial);
   const [horasExtraState, setHorasExtraState] = useState(0);
   const [horasRecargoNocturnoState, setHorasRecargoNocturnoState] = useState(0);
+  const [horasTotalesState, setHorasTotalesState] = useState(0);
 
   const editarFormulario = (e) => {
     setFormulario({
@@ -78,17 +44,36 @@ function App() {
     return totalHoras > jornadaLaboral ? totalHoras - jornadaLaboral : 0;
   };
 
+  const calcHorasTotales = (entradaEnMilisegundos, salidaEnMilisegundos) => {
+    let total;
+    if (salidaEnMilisegundos > entradaEnMilisegundos) {
+      total = (salidaEnMilisegundos - entradaEnMilisegundos) / 1000 / 3600;
+    } else {
+      total = (salidaEnMilisegundos - entradaEnMilisegundos) / 1000 / 3600;
+      if (total < 0) total += 24;
+    }
+    return total;
+  };
+
   const calcRecargoNocturno = (
     entradaString,
     salidaString,
     entradaMilisegundos,
     salidaMilisegundos
   ) => {
-    const fechaEntrada = darFormatoFecha(entradaString); //object
-    const fechaSalida =
+    let fechaEntrada = darFormatoFecha(entradaString);
+    let fechaSalida =
       entradaMilisegundos > salidaMilisegundos
         ? darFormatoFecha(salidaString, 1)
         : darFormatoFecha(salidaString, 0);
+
+    if (
+      fechaEntrada < inicioRecargoNocturno &&
+      fechaSalida < inicioRecargoNocturno
+    ) {
+      fechaEntrada = darFormatoFecha(entradaString, 1);
+      fechaSalida = darFormatoFecha(salidaString, 1);
+    }
 
     if (
       fechaEntrada <= finRecargoNocturno &&
@@ -106,6 +91,7 @@ function App() {
   const calcularHoras = () => {
     let horasExtras = 0;
     let horasRecargoNocturno = 0;
+    let horasTotales = 0;
 
     for (let i = 1; i <= dias.length; i++) {
       const entradaMilisegundos = darFormatoFecha(formulario[`entradaDia${i}`]);
@@ -119,44 +105,28 @@ function App() {
         entradaMilisegundos,
         salidaMilisegundos
       );
+
+      horasTotales += calcHorasTotales(entradaMilisegundos, salidaMilisegundos);
     }
     setHorasRecargoNocturnoState(horasRecargoNocturno);
     setHorasExtraState(horasExtras);
+    setHorasTotalesState(horasTotales);
   };
 
   const inicioRecargoNocturno = darFormatoFecha("21:00", 0);
   const finRecargoNocturno = darFormatoFecha("06:00", 1);
 
-  // --------------------------- RENDER --------------------------------------
   return (
     <div className="container py-5">
       <div className="row">
         <div className="col">
           <h1>Calculadora de recargo nocturno y horas extras</h1>
 
-          {/* <label htmlFor="inicioRecargo">Inicio recargo nocturno:</label>
-<input
-  type="time"
-  id="inicioRecargo"
-  name="inicioRecargo"
-  value={inicioRecargo}
-  onChange={(e) => setInicioRecargo(e.target.value)}
-/>
-
-<label htmlFor="finRecargo">Fin recargo nocturno:</label>
-<input
-  type="time"
-  id="finRecargo"
-  name="finRecargo"
-  value={finRecargo}
-  onChange={(e) => setFinRecargo(e.target.value)}
-/> */}
-
           <label className="form-label" htmlFor="jornadaLaboral">
             Jornada laboral en horas:{" "}
           </label>
           <input
-            className="form-control" //size
+            className="form-control"
             type="number"
             id="jornadaLaboral"
             name="jornadaLaboral"
@@ -170,9 +140,9 @@ function App() {
         <div className="col-8 border p-3">
           {dias.map((dia, index) => {
             return (
-              <div class="row mt-3" key={index}>
+              <div className="row mt-3" key={index}>
                 <p>Dia {dia}</p>
-                <div class="col">
+                <div className="col">
                   <label className="form-label" htmlFor={`entradaDia${dia}`}>
                     Hora entrada:{" "}
                   </label>
@@ -184,7 +154,7 @@ function App() {
                     onChange={editarFormulario}
                   />
                 </div>
-                <div class="col">
+                <div className="col">
                   <label htmlFor={`salidaDia${dia}`} className="form-label">
                     Hora salida:{" "}
                   </label>
@@ -213,23 +183,13 @@ function App() {
             El total de horas con recargo nocturno fueron{" "}
             <span className="text-warning">{horasRecargoNocturnoState}</span>
           </h4>
+
+          <h4>
+            El total de horas fueron{" "}
+            <span className="text-warning">{horasTotalesState}</span>
+          </h4>
         </div>
       </div>
-
-      {/* <table>
-  <thead>
-    <tr>
-      <th>Trabajador</th>
-      <th>Ingreso</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <td>Cell 1</td>
-      <td>Cell 2</td>
-    </tr>
-  </tbody>
-</table> */}
     </div>
   );
 }
